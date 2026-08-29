4e603ecdfc0208dae091cfc2e23032d97ff74cb4
ayamsem <raymondsaragih18@gmail.com>
2026-08-29 11:50:34 +0700

    week01 : initial setup and procedural code

---
+++ src/oop_132609_Raymond_Jacob_Apanov_Saragih/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_132609_Raymond_Jacob_Apanov_Saragih.week01
+
+fun main() {
+    var name: String = "Raymond Jacob Apanov Saragih"
+    var score: Int = 80
+
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
